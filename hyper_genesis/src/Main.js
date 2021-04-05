468c61b0b32986419e47f64bbd0c618f46f30286
Zachary <zacharytjones1@gmail.com>
2021-04-05 10:46:49 -0500

    Demonstration commit.

--- hyper_genesis/src/Main.js
+++ hyper_genesis/src/Main.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { useEffect, useState } from "react";
 
 const Main = () => {
-    // main logic up herek
+    // main logic up here
     const [map, setMap] = useState([]);
     useEffect(() => {
         console.log("Things happening");
     }, [map])
+
+    // GM is used to reach out to the 
     async function GenerateMapola(){
         console.log("Generate mapola!");
         await fetch("https://localhost:5001/GenRoom")
@@ -25,8 +27,8 @@ const Main = () => {
             map[y][x] = 1;
         }
         // await setMap(map);
-        setMap([...map]);
-        console.log(map);
+        setMap([...map]); // easiest way to make this work, change map, then put the changed map into set map using ...map
+        console.log(map); // ... is used to populate the list using a different list since these things aren't 1-1
         console.log("things are supposed to change");
     }
     
